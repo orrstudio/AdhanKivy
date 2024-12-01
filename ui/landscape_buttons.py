b7794b9c45921a665825c38829392172a3f3ed4e
Oruj Gafarov <65281943+orrstudio@users.noreply.github.com>
2024-12-01 20:00:25 +0400

    Add landscape_buttons.py to previous commit

--- ui/landscape_buttons.py
+++ ui/landscape_buttons.py
@@ -1,6 +1,20 @@
 from kivy.uix.floatlayout import FloatLayout
+from ui.settings_window import SettingsWindow
+from data.database import SettingsDatabase
 
 class LandscapeButtonsLayout(FloatLayout):
-    def __init__(self, **kwargs):
+    def __init__(self, clock_label=None, **kwargs):
         super().__init__(**kwargs)
-        # Пока оставляем пустым для будущей реализации
+        self.db = SettingsDatabase()
+        self.clock_label = clock_label
+
+    def open_settings_window(self, *args):
+        """Показать окно настроек"""
+        print("Landscape open_settings_window called!")  # Отладочная печать
+        settings_window = SettingsWindow(self.db, self, self.apply_settings)
+        settings_window.open()
+
+    def apply_settings(self, color_tuple):
+        """Применить настройки цвета к часам"""
+        if hasattr(self.clock_label, 'color'):
+            self.clock_label.color = color_tuple
