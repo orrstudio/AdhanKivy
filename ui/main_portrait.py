eafcc71d654364f76ad00441d6a11d5df87eedea
Oruj Gafarov <65281943+orrstudio@users.noreply.github.com>
2024-12-05 01:45:38 +0400

    НАКОНЕЦТО С ДИЗАЙНОМ ЮИ ВСЕ ГОТОВО

--- ui/main_portrait.py
+++ ui/main_portrait.py
@@ -3,20 +3,21 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from datetime import datetime
 import locale
+from ui.main_portrait_prayer_times import create_prayer_times_layout, create_next_time_layout
 
 def create_line_label(base_font_size):
     return Label(
-        text='―' * 150,  # Много тире
+        text='―' * 150,  # Много тире для линии
         font_name='PrayerNameFont',
-        height=base_font_size * 0.1, # Высота пропорциональна базовому шрифту
-        size_hint_y=None,  # Фиксированная высота
+        height=base_font_size * 0.1, # Фиксированная высота
+        size_hint_y=None,  # Нужно для фиксированной высоты
     )
 
 def create_space_label(base_font_size):
     return Label(
         text=' ', 
-        height=base_font_size * 0.02,  # Очень маленькая фиксированная высота
-        size_hint_y=None  # Не растягивается
+        height=base_font_size * 0.02,  # Фиксированная высота
+        size_hint_y=None  # Нужно для фиксированной высоты
     )
 
 def create_portrait_widgets(self, portrait_layout):
@@ -46,60 +47,30 @@ def create_portrait_widgets(self, portrait_layout):
     date_hijri_label = Label(
         text=formatted_date_xijri,
         font_name='PrayerNameFont',
-        font_size=base_font_size * 0.25,  # Размер шрифта пропорционально базовому
+        font_size=base_font_size * 0.2,  # Размер шрифта
         color=(1, 1, 1, 1),  # Белый цвет
         size_hint_x=1,  # Занимает всю ширину
         size_hint_y=None,  # Фиксированная высота
-        height=base_font_size * 0.3,  # Высота пропорциональна базовому шрифту
-        halign='center',  # Центрирование по горизонтали
-        valign='middle',  # Центрирование по вертикали
+        height=base_font_size * 0.25,  # Фиксированная высота
+        halign='center',  # Центр по горизонтали
+        valign='middle',  # Центр по вертикали
     )
 
     # Создаем Label для даты Григорианской
     date_gregorian_label = Label(
         text=formatted_date_gregorian,
         font_name='PrayerNameFont',
-        font_size=base_font_size * 0.25,  # Размер шрифта пропорционально базовому
+        font_size=base_font_size * 0.2,  # Размер шрифта
         color=(1, 1, 1, 1),  # Белый цвет
         size_hint_x=1,  # Занимает всю ширину
         size_hint_y=None,  # Фиксированная высота
-        height=base_font_size * 0.3,  # Высота пропорциональна базовому шрифту
-        halign='center',  # Центрирование по горизонтали
-        valign='middle',  # Центрирование по вертикали
+        height=base_font_size * 0.25,  # Фиксированная высота
+        halign='center',  # Центр по горизонтали
+        valign='middle',  # Центр по вертикали
     )
     
     # Создаем GridLayout для NextTimeName и NextTimeNumbers
-    nex_time_layout = GridLayout(
-        cols=2,  # Два столбца
-        size_hint_x=1,  # Занимает всю ширину
-        size_hint_y=None,  # Фиксированная высота
-        height=base_font_size * 0.6,  # Высота пропорциональна базовому шрифту
-        spacing=(10, 0)  # Небольшой отступ между элементами
-    )
-
-    # Создаем Label для NextTimeName
-    next_time_name_label = Label(
-        text='NextTime -',
-        font_name='PrayerNameFont',
-        font_size=base_font_size * 0.3,  # Маленький размер
-        color=(1, 1, 1, 1),  # Белый цвет
-        halign='left',  # Выравнивание вправо
-    )
-
-    # Создаем Label для NextTimeNumbers
-    next_time_numbers_label = Label(
-        text='00:00',
-        font_name='PrayerTimeFont',  # Шрифт как у часиков
-        font_size=base_font_size * 0.5,  # Большой размер шрифта
-        color=(1, 1, 1, 1),  # Белый цвет
-        size_hint_x=1,  # Занимает всю ширину
-        height=base_font_size * 0.6,  # Высота пропорциональна базовому шрифту
-        halign='right',  # Выравнивание влево
-    )
-
-    # Добавляем Label в GridLayout
-    nex_time_layout.add_widget(next_time_name_label)
-    nex_time_layout.add_widget(next_time_numbers_label)
+    nex_time_layout = create_next_time_layout(self, base_font_size)
 
     # Добавляем виджеты в layout в нужном порядке
     portrait_layout.add_widget(create_space_label(base_font_size))
@@ -108,5 +79,9 @@ def create_portrait_widgets(self, portrait_layout):
     portrait_layout.add_widget(create_line_label(base_font_size))  # line_label2
     portrait_layout.add_widget(nex_time_layout)
     portrait_layout.add_widget(create_line_label(base_font_size))  # line_label2
-
+    
+    # Добавляем layout с временами молитв
+    prayer_times_layout = create_prayer_times_layout(self, base_font_size)
+    portrait_layout.add_widget(prayer_times_layout)
+    
     return portrait_layout
